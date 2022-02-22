3cc1bca0bffe439c540e91197325dde99a0bdb3f
Andy Lyons <lyons.andy@gmail.com>
2022-02-21 23:18:15 -0800

    Initial commit

---
+++ Part1.R
@@ -0,0 +1,114 @@
+## Enter these commands *at the CONSOLE*
+
+3 * 7
+
+4 ^ 4
+
+## Order of operations
+
+3 + 5 ^ 2
+
+(3 + 5) ^ 2
+
+## Constants
+
+pi
+
+## Save results of expressions to objects
+
+x = 2 * 7
+
+y = 3 + 4
+
+x
+
+y
+
+x + y
+
+x + y ^ 2
+
+z = x + y ^ 2
+
+z
+
+z = 32
+
+z
+
+z = z ^ 2
+
+z
+
+## Look what happens with an incomplete expression
+
+z = 5 + 
+
+## Comparison operators
+
+x > 5
+
+x < 14 
+
+x <= 14 
+
+x > y
+
+x >= y * 2
+
+x = y   ## oh no, we just redefined x !!!!
+
+x = 14
+
+x == y
+
+x == y * 2
+
+## Naming objects
+
+x1 = 22
+
+## Camel case / Upper-lower case  
+ShoeSize = 14
+
+shoe_size = 12
+
+shoe_size
+
+SHOE_SIZE
+
+shoe-size = 9.5
+
+shoe.size = 9.5
+
+## Character values
+
+shoe_color = "brown"
+
+shoe_color
+
+shoe_color + shoe_size
+
+## Challenge. Write an expression that returns the area of a circle whose diameter is 10m
+
+pi * (10 / 2) ^ 2
+
+## ans 78.54
+
+## Data Types
+
+## All R objects have a class / type
+## You can view the class with:
+
+class(shoe_color)
+
+class(shoe_size)
+
+## Teaser
+
+hist(rnorm(1000))
+
+## END OF PART 1
+## GO BACK TO SLIDES, SHOW ZOOM POLL, THEN TAKE A BREAK
+##########################################################
+
---
+++ Part3.R
@@ -0,0 +1,37 @@
+library(magrittr)
+
+## Let's create a data frame with some information about countries:
+countries = c("Canada", "Costa Rica", "Mexico", "United States")
+populations = c(10, 25, 20, 30)
+areas = c(30, 10, 20, 35)
+
+## We can create the data frame with the data.frame() function.
+## The equal-length vectors are the arguments.
+stats_df = data.frame(country = countries, pop = populations, area = areas)
+
+stats_df
+
+# How many rows? columns?
+nrow(stats_df)
+ncol(stats_df)
+
+# You can grab a single column with the $
+stats_df$pop
+
+stats_df$pop %>% mean()
+
+## Built in data frames
+
+iris
+
+head(iris)
+
+nrow(iris)
+
+View(iris)
+
+table(iris$Species)
+
+
+
+
